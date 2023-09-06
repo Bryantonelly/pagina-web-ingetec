0af43ff790f654ac0187bc52e3bb9d86118751b5
AsslyEva <apoyouti3_lima@sunarp.gob.pe>
2023-09-06 11:05:06 -0500

    carrousel v1

--- src/app/modules/nosotros/components/aniversario/aniversario.component.ts
+++ src/app/modules/nosotros/components/aniversario/aniversario.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import M from 'materialize-css';
 @Component({
   selector: 'app-aniversario',
   templateUrl: './aniversario.component.html',
-  styleUrls: ['./aniversario.component.scss']
+  styleUrls: ['./aniversario.component.scss'],
 })
-export class AniversarioComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class AniversarioComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    this.startCarousel();
   }
 
+  startCarousel() {
+    const images = document.querySelectorAll('.images img');
+    let currentIndex = 0;
+
+    setInterval(() => {
+      images[currentIndex].classList.remove('visible');
+      currentIndex = (currentIndex + 1) % images.length;
+      images[currentIndex].classList.add('visible');
+    }, 6000); // Cambia de imagen cada 2 segundos (ajusta según tu preferencia)
+  }
 }
--- src/app/modules/nosotros/nosotros.module.ts
+++ src/app/modules/nosotros/nosotros.module.ts
@@ -3,17 +3,23 @@ import { CommonModule } from '@angular/common';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { NosotrosRoutingModule } from './nosotros-routing.module';
 import { AniversarioComponent } from './components/aniversario/aniversario.component';
-
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { SobreNosotrosComponent } from './components/sobre-nosotros/sobre-nosotros.component';
+import { BrowserModule } from '@angular/platform-browser';
 @NgModule({
   declarations: [
-  
-    AniversarioComponent
+    AniversarioComponent,
+    NosotrosComponent,
+    SobreNosotrosComponent,
   ],
   imports: [
     CommonModule,
     NosotrosRoutingModule,
-  ]
+    FormsModule,
+    HttpClientModule,
+    // BrowserModule,
+  ],
 })
-export class NosotrosModule { }
+export class NosotrosModule {}
