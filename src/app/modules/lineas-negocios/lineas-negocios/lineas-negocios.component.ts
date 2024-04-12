71c64c47ec5ab2ece61aec4ed424bba62e5daaf7
Bryantonelly <bryan.antonelly.villegas.cuba@gmail.com>
2024-04-12 07:43:45 -0500

    mejora lineas de negocio

--- src/app/modules/lineas-negocios/lineas-negocios/lineas-negocios.component.ts
+++ src/app/modules/lineas-negocios/lineas-negocios/lineas-negocios.component.ts
@@ -10,14 +10,29 @@ export class LineasNegociosComponent implements OnInit {
   @ViewChild('seccion2Ref') seccion2Ref!: ElementRef;
   @ViewChild('seccion3Ref') seccion3Ref!: ElementRef;
 
-  images: string[] = [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZGCaI8fhARKmUw5nPywWWD-V5r7xdG_6CvA&usqp=CAU',
-    'https://altertecnia.com/wp-content/uploads/ingenieros-crisis-covid19.jpg',
-    'https://predictiva21.com/wp-content/uploads/2021/07/94dbb3d0-2430-4af4-9f4c-43cdbf8aaa41-1024x535.jpeg'
+  imagesOpt1: string[] = [
+    'assets\\img\\carousel\\3.jpeg',
+    'assets\\img\\carousel\\2.jpeg',
+    'assets\\img\\carousel\\1.jpeg'
   ];
 
-  imageSelect: string = this.images[0];
-  indexImage: number = 0;
+  imagesOpt2: string[] = [
+    'assets\\img\\carousel\\4.jpeg',
+    'assets\\img\\carousel\\5.jpeg',
+    'assets\\img\\carousel\\6.jpeg'
+  ];
+
+  imagesOpt3: string[] = [
+    'assets\\img\\carousel\\7.jpeg',
+    'assets\\img\\carousel\\8.jpeg',
+    'assets\\img\\carousel\\9.jpeg'
+  ];
+  imageSelectOpt1: string = this.imagesOpt1[0];
+  imageSelectOpt2: string = this.imagesOpt2[0];
+  imageSelectOpt3: string = this.imagesOpt3[0];
+  indexImage1: number = 0;
+  indexImage2: number = 0;
+  indexImage3: number = 0;
 
   ocultar= false;
 
@@ -26,24 +41,63 @@ export class LineasNegociosComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  nextImage(){
-    if( this.indexImage + 1 > this.images.length - 1 ){
-      this.indexImage = 0;
+  nextImage1(){
+    if( this.indexImage1 + 1 > this.imagesOpt1.length - 1 ){
+      this.indexImage1 = 0;
     }else{
-      this.indexImage  = this.indexImage + 1;
+      this.indexImage1  = this.indexImage1 + 1;
     }
-    console.log(this.indexImage);
-    this.imageSelect = this.images[ this.indexImage ];
+    console.log(this.indexImage1);
+    this.imageSelectOpt1 = this.imagesOpt1[ this.indexImage1 ];
   }
 
-  prevImage(){
-    if( this.indexImage - 1 < 0 ){
-      this.indexImage = this.images.length - 1;
+  nextImage2(){
+    if( this.indexImage2 + 1 > this.imagesOpt2.length - 1 ){
+      this.indexImage2 = 0;
     }else{
-      this.indexImage = this.indexImage - 1;
+      this.indexImage2  = this.indexImage2 + 1;
     }
-    console.log(this.indexImage);
-    this.imageSelect = this.images[ this.indexImage ];
+    console.log(this.indexImage2);
+    this.imageSelectOpt2 = this.imagesOpt2[ this.indexImage2 ];
+  }
+  nextImage3(){
+    if( this.indexImage3 + 1 > this.imagesOpt3.length - 1 ){
+      this.indexImage3 = 0;
+    }else{
+      this.indexImage3  = this.indexImage3 + 1;
+    }
+    console.log(this.indexImage3);
+    this.imageSelectOpt3 = this.imagesOpt3[ this.indexImage3 ];
+  }
+
+  prevImage1(){
+    if( this.indexImage1 - 1 < 0 ){
+      this.indexImage1 = this.imagesOpt1.length - 1;
+    }else{
+      this.indexImage1 = this.indexImage1 - 1;
+    }
+    console.log(this.indexImage1);
+    this.imageSelectOpt1 = this.imagesOpt1[ this.indexImage1 ];
+  }
+
+  prevImage2(){
+    if( this.indexImage2 - 1 < 0 ){
+      this.indexImage2 = this.imagesOpt2.length - 1;
+    }else{
+      this.indexImage2 = this.indexImage2 - 1;
+    }
+    console.log(this.indexImage2);
+    this.imageSelectOpt2 = this.imagesOpt2[ this.indexImage2 ];
+  }
+
+  prevImage3(){
+    if( this.indexImage3 - 1 < 0 ){
+      this.indexImage3 = this.imagesOpt3.length - 1;
+    }else{
+      this.indexImage3 = this.indexImage3 - 1;
+    }
+    console.log(this.indexImage3);
+    this.imageSelectOpt3 = this.imagesOpt3[ this.indexImage3 ];
   }
 
   scrollToSection(section: string) {
